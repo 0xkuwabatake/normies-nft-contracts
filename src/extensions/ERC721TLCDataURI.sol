905f1a82cea78cd5902db8a5d9ae762f1d34b9e3
0xkuwabatake <0xkuwabatake@gmail.com>
2024-08-14 00:43:32 +0700

    remove {Base64} lib and replace its encoding function as a private helper

--- src/extensions/ERC721TLCDataURI.sol
+++ src/extensions/ERC721TLCDataURI.sol
@@ -2,12 +2,10 @@
 pragma solidity ^0.8.4;
 
 import "./ERC721TLCToken.sol";
-import "solady/utils/Base64.sol";
 
 /// @author 0xkuwabatake (@0xkuwabatake)
 abstract contract ERC721TLCDataURI is ERC721TLCToken {
-    using Base64 for bytes;
-    
+
     ///////// STRUCT //////////////////////////////////////////////////////////////////////////////O-'
 
     /// @dev Custom type represents dataURI for tier ID NFT metadata.
@@ -57,27 +55,14 @@ abstract contract ERC721TLCDataURI is ERC721TLCToken {
             return string(
                 abi.encodePacked(
                     "data:application/json;base64,",
-                    Base64.encode(
-                        bytes(
-                            string.concat(
-                                _header(tokenId),
-                                _body(tokenId),
-                                _expiryDate(tokenId),
-                                '"}]}'
-                                )
-                        )
-                    )
+                    _toBase64(bytes(string.concat(_header(tokenId),_body(tokenId),_expiryDate(tokenId),'"}]}')))
                 )
             );
         } else {
             return string(
                 abi.encodePacked(
                     "data:application/json;base64,",
-                    Base64.encode(
-                        bytes(
-                            string.concat(_header(tokenId), _body(tokenId), '"}]}')
-                        )
-                    )
+                    _toBase64(bytes(string.concat(_header(tokenId), _body(tokenId), '"}]}')))
                 )
             );
         }
@@ -191,6 +176,69 @@ abstract contract ERC721TLCDataURI is ERC721TLCToken {
     }
 
     ///////// PRIVATE HELPER FUNCTIONS /////////
+
+    /// @dev Encodes `data` using the base64 encoding described in RFC 4648.
+    function _toBase64(bytes memory data)
+        internal
+        pure
+        returns (string memory result)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            let dataLength := mload(data)
+
+            if dataLength {
+                // Multiply by 4/3 rounded up.
+                // The `shl(2, ...)` is equivalent to multiplying by 4.
+                let encodedLength := shl(2, div(add(dataLength, 2), 3))
+
+                // Set `result` to point to the start of the free memory.
+                result := mload(0x40)
+
+                // Store the table into the scratch space.
+                // Offsetted by -1 byte so that the `mload` will load the character.
+                // We will rewrite the free memory pointer at `0x40` later with
+                // the allocated size.
+                // The magic constant 0x0670 will turn "-_" into "+/".
+                mstore(0x1f, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdef")
+                mstore(0x3f, xor("ghijklmnopqrstuvwxyz0123456789-_", mul(iszero(false), 0x0670)))
+
+                // Skip the first slot, which stores the length.
+                let ptr := add(result, 0x20)
+                let end := add(ptr, encodedLength)
+
+                let dataEnd := add(add(0x20, data), dataLength)
+                let dataEndValue := mload(dataEnd) // Cache the value at the `dataEnd` slot.
+                mstore(dataEnd, 0x00) // Zeroize the `dataEnd` slot to clear dirty bits.
+
+                // Run over the input, 3 bytes at a time.
+                for {} 1 {} {
+                    data := add(data, 3) // Advance 3 bytes.
+                    let input := mload(data)
+
+                    // Write 4 bytes. Optimized for fewer stack operations.
+                    mstore8(0, mload(and(shr(18, input), 0x3F)))
+                    mstore8(1, mload(and(shr(12, input), 0x3F)))
+                    mstore8(2, mload(and(shr(6, input), 0x3F)))
+                    mstore8(3, mload(and(input, 0x3F)))
+                    mstore(ptr, mload(0x00))
+
+                    ptr := add(ptr, 4) // Advance 4 bytes.
+                    if iszero(lt(ptr, end)) { break }
+                }
+                mstore(dataEnd, dataEndValue) // Restore the cached value at `dataEnd`.
+                mstore(0x40, add(end, 0x20)) // Allocate the memory.
+                // Equivalent to `o = [0, 2, 1][dataLength % 3]`.
+                let o := div(2, mod(dataLength, 3))
+                // Offset `ptr` and pad with '='. We can simply write over the end.
+                mstore(sub(ptr, o), shl(240, 0x3d3d))
+                // Set `o` to zero if there is padding.
+                o := mul(iszero(iszero(false)), o)
+                mstore(sub(ptr, o), 0) // Zeroize the slot after the string.
+                mstore(result, sub(encodedLength, o)) // Store the length.
+            }
+        }
+    }
 
     /// @dev Converts a uint256 to its ASCII string decimal representation.
     /// Source: https://github.com/chiru-labs/ERC721A/blob/main/contracts/ERC721A.sol#L1266
