ea4a3ce75a0ee5520e493d08de523c339f34aaf3
0xkuwabatake <0xkuwabatake@gmail.com>
2024-08-14 11:56:44 +0700

    initial commit

---
+++ src/utils/TLCLib.sol
@@ -0,0 +1,190 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+/// @author 0xkuwabatake(@0xkuwabatake)
+library TLCLib {
+
+    /// @dev Force sends all the ETH in the current contract to `to`, with a `gasStipend`.
+    /// Source: https://github.com/Vectorized/solady/blob/main/src/utils/SafeTransferLib.sol#L153
+    function forceSafeTransferAllETH(address to, uint256 gasStipend) internal {
+        /// @solidity memory-safe-assembly
+        assembly {
+            if iszero(call(gasStipend, to, selfbalance(), codesize(), 0x00, codesize(), 0x00)) {
+                mstore(0x00, to) // Store the address in scratch space.
+                mstore8(0x0b, 0x73) // Opcode `PUSH20`.
+                mstore8(0x20, 0xff) // Opcode `SELFDESTRUCT`.
+                // For gas estimation.
+                if iszero(create(selfbalance(), 0x0b, 0x16)) { revert(codesize(), codesize()) } 
+            }
+        }
+    }
+
+    /// @dev Returns whether `leaf` exists in the Merkle tree with `root`, given `proof`.
+    /// Source: https://github.com/Vectorized/solady/blob/main/src/utils/MerkleProofLib.sol#L46
+    function verifyMerkleLeaf(bytes32[] calldata proof, bytes32 root, bytes32 leaf)
+        internal
+        pure
+        returns (bool isValid)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            if proof.length {
+                // Left shift by 5 is equivalent to multiplying by 0x20.
+                let end := add(proof.offset, shl(5, proof.length))
+                // Initialize `offset` to the offset of `proof` in the calldata.
+                let offset := proof.offset
+                // Iterate over proof elements to compute root hash.
+                for {} 1 {} {
+                    // Slot of `leaf` in scratch space.
+                    // If the condition is true: 0x20, otherwise: 0x00.
+                    let scratch := shl(5, gt(leaf, calldataload(offset)))
+                    // Store elements to hash contiguously in scratch space.
+                    // Scratch space is 64 bytes (0x00 - 0x3f) and both elements are 32 bytes.
+                    mstore(scratch, leaf)
+                    mstore(xor(scratch, 0x20), calldataload(offset))
+                    // Reuse `leaf` to store the hash to reduce stack operations.
+                    leaf := keccak256(0x00, 0x40)
+                    offset := add(offset, 0x20)
+                    if iszero(lt(offset, end)) { break }
+                }
+            }
+            isValid := eq(leaf, root)
+        }
+    }
+
+    /// @dev Encodes `data` using the base64 encoding described in RFC 4648.
+    /// Source: https://github.com/Vectorized/solady/blob/main/src/utils/Base64.sol#L13
+    function toBase64(bytes memory data)
+        internal
+        pure
+        returns (string memory result)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            let dataLength := mload(data)
+
+            if dataLength {
+                // Multiply by 4/3 rounded up.
+                // The `shl(2, ...)` is equivalent to multiplying by 4.
+                let encodedLength := shl(2, div(add(dataLength, 2), 3))
+
+                // Set `result` to point to the start of the free memory.
+                result := mload(0x40)
+
+                // Store the table into the scratch space.
+                // Offsetted by -1 byte so that the `mload` will load the character.
+                // We will rewrite the free memory pointer at `0x40` later with
+                // the allocated size.
+                // The magic constant 0x0670 will turn "-_" into "+/".
+                mstore(0x1f, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdef")
+                mstore(0x3f, xor("ghijklmnopqrstuvwxyz0123456789-_", mul(iszero(false), 0x0670)))
+
+                // Skip the first slot, which stores the length.
+                let ptr := add(result, 0x20)
+                let end := add(ptr, encodedLength)
+
+                let dataEnd := add(add(0x20, data), dataLength)
+                let dataEndValue := mload(dataEnd) // Cache the value at the `dataEnd` slot.
+                mstore(dataEnd, 0x00) // Zeroize the `dataEnd` slot to clear dirty bits.
+
+                // Run over the input, 3 bytes at a time.
+                for {} 1 {} {
+                    data := add(data, 3) // Advance 3 bytes.
+                    let input := mload(data)
+
+                    // Write 4 bytes. Optimized for fewer stack operations.
+                    mstore8(0, mload(and(shr(18, input), 0x3F)))
+                    mstore8(1, mload(and(shr(12, input), 0x3F)))
+                    mstore8(2, mload(and(shr(6, input), 0x3F)))
+                    mstore8(3, mload(and(input, 0x3F)))
+                    mstore(ptr, mload(0x00))
+
+                    ptr := add(ptr, 4) // Advance 4 bytes.
+                    if iszero(lt(ptr, end)) { break }
+                }
+                mstore(dataEnd, dataEndValue) // Restore the cached value at `dataEnd`.
+                mstore(0x40, add(end, 0x20)) // Allocate the memory.
+                // Equivalent to `o = [0, 2, 1][dataLength % 3]`.
+                let o := div(2, mod(dataLength, 3))
+                // Offset `ptr` and pad with '='. We can simply write over the end.
+                mstore(sub(ptr, o), shl(240, 0x3d3d))
+                // Set `o` to zero if there is padding.
+                o := mul(iszero(iszero(false)), o)
+                mstore(sub(ptr, o), 0) // Zeroize the slot after the string.
+                mstore(result, sub(encodedLength, o)) // Store the length.
+            }
+        }
+    }
+
+    /// @dev Converts a uint256 to its ASCII string decimal representation.
+    /// Source: https://github.com/chiru-labs/ERC721A/blob/main/contracts/ERC721A.sol#L1266
+    function toString(uint256 value) internal pure returns (string memory str) {
+        assembly {
+            // The maximum value of a uint256 contains 78 digits (1 byte per digit), but
+            // we allocate 0xa0 bytes to keep the free memory pointer 32-byte word aligned.
+            // We will need 1 word for the trailing zeros padding, 1 word for the length,
+            // and 3 words for a maximum of 78 digits. Total: 5 * 0x20 = 0xa0.
+            let m := add(mload(0x40), 0xa0)
+            // Update the free memory pointer to allocate.
+            mstore(0x40, m)
+            // Assign the `str` to the end.
+            str := sub(m, 0x20)
+            // Zeroize the slot after the string.
+            mstore(str, 0)
+
+            // Cache the end of the memory to calculate the length later.
+            let end := str
+
+            // We write the string from rightmost digit to leftmost digit.
+            // The following is essentially a do-while loop that also handles the zero case.
+            // prettier-ignore
+            for { let temp := value } 1 {} {
+                str := sub(str, 1)
+                // Write the character to the pointer.
+                // The ASCII index of the '0' character is 48.
+                mstore8(str, add(48, mod(temp, 10)))
+                // Keep dividing `temp` until zero.
+                temp := div(temp, 10)
+                // prettier-ignore
+                if iszero(temp) { break }
+            }
+
+            let length := sub(end, str)
+            // Move the pointer 32 bytes leftwards to make room for the length.
+            str := sub(str, 0x20)
+            // Store the length.
+            mstore(str, length)
+        }
+    }
+
+    /// @dev Returns (`year`,`month`,`day`) from the given unix timestamp.
+    /// Source: https://github.com/Vectorized/solady/blob/main/src/utils/DateTimeLib.sol#L118
+    function timestampToDate(uint256 timestamp)
+        internal
+        pure
+        returns (uint256 year, uint256 month, uint256 day)
+    {
+        (year, month, day) = epochDayToDate(timestamp / 86400);
+    }
+
+    /// @dev Returns (`year`,`month`,`day`) from the number of days since 1970-01-01.
+    /// Source: https://github.com/Vectorized/solady/blob/main/src/utils/DateTimeLib.sol#L83
+    function epochDayToDate(uint256 epochDay)
+        internal
+        pure
+        returns (uint256 year, uint256 month, uint256 day)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            epochDay := add(epochDay, 719468)
+            let doe := mod(epochDay, 146097)
+            let yoe :=
+                div(sub(sub(add(doe, div(doe, 36524)), div(doe, 1460)), eq(doe, 146096)), 365)
+            let doy := sub(doe, sub(add(mul(365, yoe), shr(2, yoe)), div(yoe, 100)))
+            let mp := div(add(mul(5, doy), 2), 153)
+            day := add(sub(doy, shr(11, add(mul(mp, 62719), 769))), 1)
+            month := byte(mp, shl(160, 0x030405060708090a0b0c0102))
+            year := add(add(yoe, mul(div(epochDay, 146097), 400)), lt(month, 3))
+        }
+    }
+}
